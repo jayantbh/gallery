beb0d080a916032354a391a3b9700649d21db47f
jayantbh <bhawal.jayant@gmail.com>
2015-03-29 02:17:16 +0530

    A 3D Spinning image carousel
    Made using only CSS (LESS).

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>3D Cube</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/less" href="css/style.less">
+
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/less.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="circlebase"></div>
+		<div class="frame f0"></div>
+		<div class="frame f1"></div>
+		<div class="frame f2"></div>
+		<div class="frame f3"></div>
+		<div class="frame f4"></div>
+		<div class="frame f5"></div>
+		<div class="frame f6"></div>
+		<div class="frame f7"></div>
+	</div>
+</body>
+</html>
